0c80aad06402aae5202ebc55a8bf856a312caab6
Arran Woodruff <arranwoodruff@gmail.com>
2025-05-24 19:49:46 -0400

    ?routing?

--- app/routes.ts
+++ app/routes.ts
@@ -6,6 +6,8 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
+	index("routes/home.tsx"),
+	route("callback", "routes/callback.tsx"),
 	...prefix("spotify-faves", [
 		index("routes/home.tsx"),
 		route("callback", "routes/callback.tsx"),
